936babbdfae11cec767ad520cd5823327d7118ff
Jarno N. Alanko <alanko.jarno@gmail.com>
2023-09-09 10:10:11 +0300

    Done but untested

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use jseqio::seq_db::SeqDB;
 use jseqio::reader::DynamicFastXReader;
 use jseqio::record::{OwnedRecord, RefRecord};
+use jseqio::writer;
 use std::collections::HashMap;
 use std::hash::Hash;
 
@@ -8,6 +9,15 @@ use std::hash::Hash;
 enum Orientation{
     Forward,
     Reverse,
+}
+
+impl Orientation {
+    fn flip(&self) -> Orientation {
+        match self {
+            Orientation::Forward => Orientation::Reverse,
+            Orientation::Reverse => Orientation::Forward,
+        }
+    }
 }
 
 #[derive(Copy, Clone, Debug)]
@@ -172,11 +182,58 @@ fn build_dbg(unitigs: SeqDB, k: usize) -> DBG{
     DBG {unitigs, edges}
 }
 
+fn pick_orientations(dbg: &DBG) -> Vec<Orientation>{
+    let mut orientations = Vec::<Orientation>::new();
+    orientations.resize(dbg.unitigs.sequence_count(), Orientation::Forward);
+
+    let mut visited = vec![false; dbg.unitigs.sequence_count()];
+
+    let mut stack = Vec::<(usize, Orientation)>::new();
+    stack.push((0, Orientation::Forward));
+
+    while let Some((unitig_id, orientation)) = stack.pop(){
+        if visited[unitig_id]{
+            continue;
+        }
+        visited[unitig_id] = true;
+        orientations[unitig_id] = orientation;
+
+        for edge in dbg.edges[unitig_id].iter(){
+            let next_orientation = match (edge.from_orientation, edge.to_orientation){
+                (Orientation::Forward, Orientation::Forward) => orientation,
+                (Orientation::Forward, Orientation::Reverse) => orientation.flip(),
+                (Orientation::Reverse, Orientation::Forward) => orientation.flip(),
+                (Orientation::Reverse, Orientation::Reverse) => orientation,
+            };
+            stack.push((edge.to, next_orientation));
+        }
+    }
+
+    orientations
+}
+
 fn main() {
     let filename = std::env::args().nth(1).unwrap();
     let k = std::env::args().nth(2).unwrap().parse::<usize>().unwrap();
     let reader = DynamicFastXReader::from_file(&filename).unwrap();
+    let filetype = reader.filetype();
     let db = reader.into_db().unwrap();
     let dbg = build_dbg(db, k);
-    dbg!(dbg.edges);
+    let orientations = pick_orientations(&dbg);
+
+    // Todo: gzip
+    let mut writer = jseqio::writer::DynamicFastXWriter::new_to_stdout(filetype, false);
+    for i in 0..dbg.unitigs.sequence_count(){
+        let orientation = orientations[i];
+        let rec: OwnedRecord = match orientation{
+            Orientation::Forward => dbg.unitigs.get(i).unwrap().to_owned(),
+            Orientation::Reverse => {
+                let mut unitig = dbg.unitigs.get(i).unwrap().to_owned();
+                unitig.seq = unitig.seq.iter().rev().map(|&c| rc(c)).collect(); // Reverse complement
+                unitig
+            }
+        };
+        writer.write(&rec);
+    }
+
 }
